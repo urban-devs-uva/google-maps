a89a0611f1d2ceca54fc4634177c2ffcb1f7b14c
sari <saricermaros@gmail.com>
2023-11-01 15:00:46 +0100

    doing it with gmaps distance matrix (doesnt work)

---
+++ learn.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.DataFrame()
+
+labels = ["a", "b", "c", "d"]
+labels2 = ["e", "f", "g"]
+
+series1 = pd.Series([1, 2, 3])
+series2 = pd.Series([4, 5, 6])
+
+
+df = pd.concat([df, series1], axis=1)
+df = pd.concat([df, series2], axis=1)
+
+df2 = df * 2
+df = pd.concat([df, df2], axis=1)
+
+df3 = pd.DataFrame()
+df3 = pd.concat([df3, df])
+df3 = pd.concat([df3, df])
+
+print(df3)
--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import googlemaps
 import pandas as pd
+import itertools
 
-gmaps = googlemaps.Client(key='AIzaSyAyZ5x-tm9dSJc2Xk4iS5A1Amh2jGQi8DM')
+gmaps = googlemaps.Client(key="AIzaSyAyZ5x-tm9dSJc2Xk4iS5A1Amh2jGQi8DM")
 
 hub_addresses = []
 with open("data/mobility_hub_addresses.txt") as file:
     lines = file.readlines()
     hub_addresses = [line.replace("\n", "") + " Amsterdam" for line in lines]
 
+hubs_batches = itertools.batched(hub_addresses, 10)
+
 train_stations = []
 with open("data/train_stations.txt") as file:
     lines = file.readlines()
     train_stations = [line.replace("\n", "") for line in lines]
 
+train_stations_batches = itertools.batched(train_stations, 10)
+
 metro_tram_df = pd.read_csv("data/TRAMMETRO_PUNTEN_2022.csv", sep=";")
 metro_df = metro_tram_df.loc[metro_tram_df["Modaliteit"] == "Metro"]
 tram_df = metro_tram_df.loc[metro_tram_df["Modaliteit"] == "Tram"]
 
-metro_coordinates = [
-    [row["LNG"], row["LAT"]]
-    for _, row in metro_df.iterrows()
-]
+metro_coordinates = [[row["LNG"], row["LAT"]] for _, row in metro_df.iterrows()]
 
 df_hubs_data = pd.DataFrame({"Mobility hub": hub_addresses})
 
-def find_nearest_from_destination_list(hub, destinations, mode, test=False):
+
+def find_nearest_from_destination_list(hub, destinations, mode):
     nearest_destination = ""
     distance_to_nearest = 999999999
     for destination in destinations:
@@ -33,10 +36,8 @@ def find_nearest_from_destination_list(hub, destinations, mode, test=False):
         if distance < distance_to_nearest:
             distance_to_nearest = distance
             nearest_destination = destination
-    return {
-        "name": nearest_destination,
-        "distance": distance_to_nearest
-    }
+    return {"name": nearest_destination, "distance": distance_to_nearest}
+
 
 nearest_stations = {
     "walking": [
@@ -46,12 +47,12 @@ nearest_stations = {
     "biking": [
         find_nearest_from_destination_list(hub, train_stations, "bicycling")
         for hub in hub_addresses
-    ]
+    ],
 }
 
 df_hubs_data["Nearest station (walking)"] = [
     station["name"] for station in nearest_stations["walking"]
-    ]
+]
 df_hubs_data["Distance to nearest train station (walking)"] = [
     station["distance"] for station in nearest_stations["walking"]
 ]
@@ -64,7 +65,7 @@ df_hubs_data["Distance to nearest train station (biking)"] = [
 
 df_hubs_data.to_csv("results/mobility_hub_google_maps_data.csv")
 
-'''
+"""
 The result should be dataset with each mobility hub per row and some new observations
 derived from this per column.
 
@@ -86,4 +87,4 @@ Other ideas (harder to get):
 - distance to university
 - 
 
-'''
+"""
